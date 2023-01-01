5e6c68dd5e3206ef61a3c00d535e1af95cf609ed
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-01-01 22:26:59 +0000

    Added routes

--- app/src/main/java/com/example/zenmuzic/routeRecycleView/Route.java
+++ app/src/main/java/com/example/zenmuzic/routeRecycleView/Route.java
@@ -1,13 +1,18 @@
 package com.example.zenmuzic.routeRecycleView;
 
 import com.example.zenmuzic.playlistRecyclerView.Playlist;
+import com.google.android.libraries.places.api.model.Place;
 
 import java.io.Serializable;
 
 public class Route implements Serializable {
-    private String name;
+    private String name = "";
 
     private Playlist playlist;
+
+    private Place startingPoint;
+
+    private Place endPoint;
 
     public String getName() {
         return name;
@@ -23,6 +28,22 @@ public class Route implements Serializable {
 
     public void setPlaylist(Playlist playlist) {
         this.playlist = playlist;
+    }
+
+    public Place getStartingPoint() {
+        return startingPoint;
+    }
+
+    public void setStartingPoint(Place startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public Place getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(Place endPoint) {
+        this.endPoint = endPoint;
     }
 
 
