fab60d103dba808f135f698481f7a8cb2e6d7aa7
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-01-02 18:18:40 +0000

    Made route parcelable

--- app/src/main/java/com/example/zenmuzic/routeRecycleView/Route.java
+++ app/src/main/java/com/example/zenmuzic/routeRecycleView/Route.java
@@ -1,11 +1,12 @@
 package com.example.zenmuzic.routeRecycleView;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.example.zenmuzic.playlistRecyclerView.Playlist;
 import com.google.android.libraries.places.api.model.Place;
 
-import java.io.Serializable;
-
-public class Route implements Serializable {
+public class Route implements Parcelable {
     private String name = "";
 
     private Playlist playlist;
@@ -13,6 +14,27 @@ public class Route implements Serializable {
     private Place startingPoint;
 
     private Place endPoint;
+
+    protected Route() {}
+
+    protected Route(Parcel in) {
+        name = in.readString();
+        playlist = in.readParcelable(Playlist.class.getClassLoader());
+        startingPoint = in.readParcelable(Place.class.getClassLoader());
+        endPoint = in.readParcelable(Place.class.getClassLoader());
+    }
+
+    public static final Creator<Route> CREATOR = new Creator<Route>() {
+        @Override
+        public Route createFromParcel(Parcel in) {
+            return new Route(in);
+        }
+
+        @Override
+        public Route[] newArray(int size) {
+            return new Route[size];
+        }
+    };
 
     public String getName() {
         return name;
@@ -47,4 +69,16 @@ public class Route implements Serializable {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeParcelable(playlist, i);
+        parcel.writeParcelable(startingPoint, i);
+        parcel.writeParcelable(endPoint, i);
+    }
 }
