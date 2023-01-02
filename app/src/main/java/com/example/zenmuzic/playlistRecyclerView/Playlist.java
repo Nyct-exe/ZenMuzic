67f669a1acf5b03c0adcb7aa6cf9362157b054fe
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-01-02 18:18:16 +0000

    Made playlist parcelable

--- app/src/main/java/com/example/zenmuzic/playlistRecyclerView/Playlist.java
+++ app/src/main/java/com/example/zenmuzic/playlistRecyclerView/Playlist.java
@@ -1,12 +1,32 @@
 package com.example.zenmuzic.playlistRecyclerView;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Playlist implements Serializable {
+public class Playlist implements Parcelable {
 
     private boolean isChecked = false;
     private String name;
     private String uri;
+
+    protected Playlist() {}
+    protected Playlist(Parcel in) {
+        isChecked = in.readByte() != 0;
+        name = in.readString();
+        uri = in.readString();
+    }
+
+    public static final Creator<Playlist> CREATOR = new Creator<Playlist>() {
+        @Override
+        public Playlist createFromParcel(Parcel in) {
+            return new Playlist(in);
+        }
+
+        @Override
+        public Playlist[] newArray(int size) {
+            return new Playlist[size];
+        }
+    };
 
     public boolean isChecked() {
         return isChecked;
@@ -30,5 +50,17 @@ public class Playlist implements Serializable {
 
     public void setUri(String uri) {
         this.uri = uri;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeByte((byte) (isChecked ? 1 : 0));
+        parcel.writeString(name);
+        parcel.writeString(uri);
     }
 }
